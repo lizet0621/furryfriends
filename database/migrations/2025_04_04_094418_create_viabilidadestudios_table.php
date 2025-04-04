af90cccc46441b2623f2079f55015b8375d300cf
Tyr-10 <juanpablovalencia457@gmail.com>
2025-04-04 04:45:02 -0500

    creacion de la tabla viabilidadestudios

--- database/migrations/2025_04_04_094418_create_viabilidadestudios_table.php
+++ database/migrations/2025_04_04_094418_create_viabilidadestudios_table.php
@@ -10,19 +10,15 @@ return new class extends Migration
     {
         Schema::create('ViabilidadEstudios', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('adoptante_id');
-            $table->unsignedBigInteger('refugio_id');
+            $table->foreignId('adoptante_id')->constrained('roles')->onDelete('cascade'); // Relacionado con roles
+            $table->foreignId('refugio_id')->constrained('roles')->onDelete('cascade');   // Relacionado con roles
             $table->string('archivo'); // Guardará la ruta del archivo
             $table->timestamps();
-
-            // Relaciones
-            $table->foreign('adoptante_id')->references('id')->on('adoptantes')->onDelete('cascade');
-            $table->foreign('refugio_id')->references('id')->on('refugios')->onDelete('cascade');
         });
     }
 
     public function down(): void
     {
         Schema::dropIfExists('ViabilidadEstudios');
-    }
+}
 };
