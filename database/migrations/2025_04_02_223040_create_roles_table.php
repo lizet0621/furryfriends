ddd0929e9a6be0bf1dae1252770b38aa3d4642b5
lizet0621 <lizle2727@gmail.com>
2025-04-05 11:46:28 -0500

    roles guardado

--- database/migrations/2025_04_02_223040_create_roles_table.php
+++ database/migrations/2025_04_02_223040_create_roles_table.php
@@ -7,30 +7,14 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('telefono')->nullable();
-            $table->string('direccion')->nullable();
-            $table->string('ciudad')->nullable();
-            $table->integer('capacidad')->nullable();
-            $table->string('horarios')->nullable();
-            $table->string('responsable')->nullable();
-            $table->text('servicios')->nullable();
-            $table->boolean('activo')->default(true);
+            $table->string('nombre', 50)->unique();
             $table->timestamps();
-
-            // Clave foránea con cascada
-            $table->unsignedBigInteger('id_rol');
-
-            $table->foreign('id_rol')->references('id')->on('roles')->onDelete('cascade');
         });
     }
-
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
     }
 };
