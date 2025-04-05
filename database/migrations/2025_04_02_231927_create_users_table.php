96d3303a87ed169bcbd3fa47ed047d6d8531b4d9
lizet0621 <lizle2727@gmail.com>
2025-04-05 11:46:48 -0500

    users nuevo

--- database/migrations/2025_04_02_231927_create_users_table.php
+++ database/migrations/2025_04_02_231927_create_users_table.php
@@ -9,22 +9,24 @@ return new class extends Migration {
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre');
+            $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
             $table->string('telefono')->nullable();
-            $table->string('direccion')->nullable(); 
+            $table->string('direccion')->nullable();
             $table->string('ciudad')->nullable();
             $table->integer('capacidad')->nullable();
             $table->string('horarios')->nullable();
             $table->string('responsable')->nullable();
             $table->text('servicios')->nullable();
+            $table->boolean('activo')->default(true);
             $table->timestamps();
 
             // Clave foránea con cascada
             $table->unsignedBigInteger('id_rol');
 
-            $table->foreign('id_rol')->references('id')->on('roles')->onDelete('restrict');        });
+            $table->foreign('id_rol')->references('id')->on('roles')->onDelete('cascade');
+        });
     }
 
     public function down(): void
