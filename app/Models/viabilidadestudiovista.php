6a58c188725262037fa8c1880a3e49e635fa39ac
lizet0621 <lizle2727@gmail.com>
2025-04-05 11:27:48 -0500

    viabilidad vista model

--- app/Models/viabilidadestudiovista.php
+++ app/Models/viabilidadestudiovista.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class ViabilidadEstudioVista extends Model
 {
     use HasFactory;
-
     protected $table = 'ViabilidadEstudios'; // Nombre real de la tabla en la BD
     protected $fillable = ['archivo'];
 }
