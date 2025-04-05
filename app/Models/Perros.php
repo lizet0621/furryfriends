a316090e434a02103a1fd79e766d65b0d8324fcd
Tyr-10 <juanpablovalencia457@gmail.com>
2025-04-05 04:12:46 -0500

    actualizacionde perros models

--- app/Models/Perros.php
+++ app/Models/Perros.php
@@ -9,10 +9,35 @@ class Perros extends Model
 {
     use HasFactory;
 
-    protected $table = 'Perros'; 
-    protected $fillable = ['nombre', 'edad', 'raza', 'tamanio', 'descripcion', 'refugio_id', 'disponible'];
+    protected $table = 'perros';
 
-    public function refugio() {
-        return $this->belongsTo(Refugio::class, 'refugio_id'); // Ajusta 'refugio_id' si usas otro nombre de clave foránea
+    protected $fillable = [
+        'nombre',
+        'edad',
+        'raza',
+        'tamanio',
+        'descripcion',
+        'refugio_id',
+        'disponible',
+        'imagenperro',
+        'sexo',
+        'historial_clinico',
+        'color',
+
+    ];
+
+    // RELACIÓN CON USUARIO REFUGIO
+    public function refugio()
+    {
+   return $this->belongsTo(Refugio::class, 'refugio_id'); // Ajusta 'refugio_id' si usas otro nombre de clave foránea
     }
+
+    public function scopeDisponibles($query)
+    {
+        return $query->where('disponible', 1);
+    }
+    public function rol()
+{
+    return $this->belongsTo(Role::class,'id_rol');
+}
 }
