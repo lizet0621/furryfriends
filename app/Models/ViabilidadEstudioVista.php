8ce0c8bb28d87640e53d0aa36d3390e9e7aa695d
Tyr-10 <juanpablovalencia457@gmail.com>
2025-04-05 04:21:16 -0500

    actualizacion de viabilidadestudiovista

--- app/Models/ViabilidadEstudioVista.php
+++ app/Models/ViabilidadEstudioVista.php
@@ -5,11 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class SeguimientoVisitasVista extends Model
+class ViabilidadEstudioVista extends Model
 {
     use HasFactory;
 
-    // Definir la tabla y los campos asignables
-    protected $table = 'seguimientoVisitas';
+    protected $table = 'ViabilidadEstudios'; // Nombre real de la tabla en la BD
     protected $fillable = ['archivo'];
 }
