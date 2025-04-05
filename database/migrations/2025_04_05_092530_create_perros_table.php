a01193e46167f0e47741c0f15db0f72a49582ce7
Tyr-10 <juanpablovalencia457@gmail.com>
2025-04-05 04:29:11 -0500

    actualizacion de tabla de migracion perros

--- database/migrations/2025_04_05_092530_create_perros_table.php
+++ database/migrations/2025_04_05_092530_create_perros_table.php
@@ -4,24 +4,40 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreatePerrosTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('perros', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
+            $table->integer('edad');
+            $table->string('imagenperro')->nullable();
+            $table->string('raza');
+            $table->string('color');
+            $table->enum('tamanio', ['Pequeño', 'Mediano', 'Grande']);
+            $table->enum('sexo', ['Macho', 'Hembra']);
+            $table->text('historial_clinico')->nullable();
+            $table->text('descripcion')->nullable();
+
+            // Foreign key hacia roles
+            $table->unsignedBigInteger('refugio_id');
+
+            // Asegura que el motor de la tabla sea InnoDB
+            $table->engine = 'InnoDB';
+
+            $table->foreign('refugio_id')
+                  ->references('id')
+                  ->on('users')
+                  ->onDelete('cascade');
+
+            $table->boolean('disponible')->default(true);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('perros');
     }
-};
+}
