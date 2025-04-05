ad9ab105b99642d0819026d6418370061795ad2e
lizet0621 <lizle2727@gmail.com>
2025-04-05 11:16:25 -0500

    nueva carpeta y codigo

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'email' => 'required|email',
+            'password' => 'required'
+        ];
+    }
+    public function getCredentials()
+    {
+        return $this->only('email', 'password');
+    }
+}
