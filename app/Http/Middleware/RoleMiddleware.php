ddc72f2b0137f783ba9cb1ef63e4692a8e429aab
lizet0621 <lizle2727@gmail.com>
2025-04-05 11:13:07 -0500

    otro

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -5,7 +5,6 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
 class RoleMiddleware
 {
     public function handle(Request $request, Closure $next, $role)
