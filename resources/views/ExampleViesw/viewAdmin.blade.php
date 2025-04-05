a7b5c393645dab87b50579b35039c3a8274c30ef
farfancha <emillycinaricicua@gmail.com>
2025-04-05 04:34:25 -0500

    agrecacion de archivos en exaple y datos

---
+++ resources/views/ExampleViesw/viewAdmin.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        Admin
+    </h1>
+
+    <form action="{{ route('logout') }}" method="POST" style="display: inline;">
+    @csrf
+    <button type="submit" class="btn btn-danger">
+        <i class="fas fa-sign-out-alt"></i> Cerrar sesión
+    </button>
+    </form>
+</body>
+</html>
---
+++ resources/views/ExampleViesw/viewNatural.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Natural
+    <form action="{{ route('logout') }}" method="POST" style="display: inline;">
+    @csrf
+    <button type="submit" class="btn btn-danger">
+        <i class="fas fa-sign-out-alt"></i> Cerrar sesión
+    </button>
+    </form>
+</body>
+</html>
