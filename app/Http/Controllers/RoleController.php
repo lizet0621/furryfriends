9aed7b1e3f9102f0f286d9ae9e255c0128214eb3
Nelson1813 <nelsonchisavo9@gmail.com>
2025-04-07 04:39:54 -0500

    cambios

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Role;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class RoleController extends Controller
+{
+    public function perfil()
+{
+    return view('perfil');
+}
+}
