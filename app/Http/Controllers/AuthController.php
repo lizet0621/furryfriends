11f39f1030f371ee49f1e3493e08d8623515a55a
Tyr-10 <juanpablovalencia457@gmail.com>
2025-04-07 04:17:22 -0500

    actualizacion de auth controlador

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -58,7 +58,6 @@ class AuthController extends Controller
             'role_id' => $request->role_id,
         ]);
 
-        
         return redirect()->route('login')->with('success', 'Registro exitoso. Por favor, inicia sesión.');
     }
 }
